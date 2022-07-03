df7bdd72e2ff8ab504971bc35636907fe996869c
Zubair199 <60172905+Zubair199@users.noreply.github.com>
2022-07-03 15:32:30 +0500

    added calender

--- screens/AddClass.tsx
+++ screens/AddClass.tsx
@@ -11,7 +11,7 @@ import RadioGroup from 'react-native-radio-buttons-group';
 import { Select, Input, TextArea, IconButton } from "native-base";
 import Icon from 'react-native-vector-icons/AntDesign';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
-
+import {Calendar} from 'react-native-calendars';
 import { useNavigation } from '@react-navigation/native';
 import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
 
@@ -512,7 +512,15 @@ export default function AddClassScreen() {
             </View>
             <View>
               <View style={{ flexDirection: "row", justifyContent: "space-evenly", marginTop: 10 }}>
-                {
+              <Calendar
+                  markedDates={{
+                    '2022-05-16': {selected: true, marked: true, selectedColor: 'blue'},
+                    '2022-05-17': {marked: true},
+                    '2022-05-18': {marked: true, dotColor: 'red', activeOpacity: 0},
+                    '2022-05-19': {disabled: true, disableTouchEvent: true}
+                  }}
+                />
+                {/* {
                   dayNames.map((item, index) => {
                     return (
                       <View style={styles.box}>
@@ -521,9 +529,8 @@ export default function AddClassScreen() {
                     )
                   })
                 }
-
-              </View>
-              {getMonthDates()}
+              {getMonthDates()} */}
+               </View>
             </View>
           </View>
         </ProgressStep>
